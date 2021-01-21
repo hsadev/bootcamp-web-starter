6fcafe9904df68047ec6f6eafe80e956afb0e9a2
Nick Castillo <nickcastillomarin@college.harvard.edu>
2021-01-20 22:20:05 -0500

    added items to search

--- src/containers/Search/styles.js
+++ src/containers/Search/styles.js
@@ -10,3 +10,12 @@ export const Row = styled.div`
   display: flex;
   flex-flow: row wrap;
 `
+
+export const ItemContainer = styled.div`
+  display: grid;
+  grid-template-columns: auto auto auto auto;
+  grid-gap: 10px;
+  padding: 10px;
+`
+
+
